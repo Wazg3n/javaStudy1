3a3c3a9ad13ec9244228c886d3199da3e28911c4
Wazg3n <arabaczjanwazgen@gmail.com>
2022-02-28 20:56:19 +0100

    CoM

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d3/c008d13c8498001c1d2dd8688c1877d6
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		String name = "";
+		
+		while(name.isBlank()) {
+			System.out.println("Please enter your name");
+			name = scanner.nextLine();
+		}
+		
+		System.out.println("Hello " +name);
+			
+			
+		}
+
+	}
+
+
---
+++ MyFirstProgram/src/mathclass.java
@@ -0,0 +1,27 @@
+
+public class mathclass {
+	
+	public static void main(String[] args) {
+		
+		double x = 3.14;
+		double y = 10;
+		
+		double z = Math.max(x, y);
+
+		System.out.println(z);
+		
+		/*Math.max compares which number is higher and min the smallest.
+		abs takes away the minus before a number for example -10 will be 10
+		sqrt = square root
+		round = rounds it up
+		
+		ceil = rounds it up always
+		floor = rounds down always
+	
+		
+		*/
+		
+		
+	}
+
+}
